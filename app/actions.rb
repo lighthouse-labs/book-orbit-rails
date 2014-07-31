cde081d2ea700f9a2a467a32cf5b3431909363c5
kennw <koonwah@gmail.com>
2014-07-30 17:51:25 -0700

    modifed actions.rb: create user if the url has never been visited

--- app/actions.rb
+++ app/actions.rb
@@ -6,6 +6,11 @@ end
 get '/:username' do
 
   name = params[:username]
+
+  if !user_exists?(name)
+    User.create(username: name)
+  end
+
   @user = User.find_by(username: name)
   erb :'/users/index'
 
--- app/helpers.rb
+++ app/helpers.rb
@@ -8,6 +8,21 @@ helpers do
     collections
   end
 
+  def all_user_collections
+    collections = []
+    BookmarksUser.where(user_id: @user.id).each do |row|
+      collections << BookmarksUsersCollection.find(row.id).collection.name
+    end
+    collections
+  end
+
+  def user_exists?(username)
+    User.all.each do |user|
+      return true if user.username == username
+    end
+    false
+  end
+
   def login?
 
   end

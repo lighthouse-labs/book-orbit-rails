94b3143d426f8658d5f93287c00cde3b5551cd60
astopo <amy.soetopo@gmx.com>
2014-08-01 14:03:02 +0000

    css js for delete button

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
   $( "fieldset" ).on("change paste keyup", "input[type='text']", function() {
     $(this).parent().siblings().show(200);
+  }); 
+  $( ".pure-u-1-3" ).on("mouseenter", function() {
+    $(this).find(".delete").show(200);
+  }).on("mouseleave",function(){
+    $(this).find(".delete").hide(200);
   });  
 });
